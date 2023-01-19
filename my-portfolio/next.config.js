4f1715bb0e7cd31009875f38c0cb85afc39edbac
Owner <ushift@DESKTOP-HHULJ21.localdomain>
2023-01-19 14:05:47 -0600

    adding to webpack config: will revert commit

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+​
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+ 
+        
+    }
 
-module.exports = nextConfig
+
